902360d7b00816caa404d311a82fbf96e114b951
Adrianr447 <adrian.rodriguez16@g.austincc.edu>
2025-09-13 16:32:32 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,18 @@
 import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.b_in_proc_out import tests_in_proc_out
+from src.homework.b_in_proc_out.output import multiply_numbers
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
-unittest.TextTestRunner(verbosity=2).run(suite)
+class TestMultiplyNumbers(unittest.TestCase):
+    def test_multiply_positive(self):
+        self.assertEqual(multiply_numbers(7, 7), 49)
+        self.assertEqual(multiply_numbers(5, 5), 25)
+
+    def test_multiply_zero(self):
+        self.assertEqual(multiply_numbers(0, 10), 0)
+        self.assertEqual(multiply_numbers(0, 0), 0)
+
+    def test_multiply_negative(self):
+        self.assertEqual(multiply_numbers(-3, 3), -9)
+        self.assertEqual(multiply_numbers(-4, -2), 8)
+
+if __name__ == "__main__":
+    unittest.main()
