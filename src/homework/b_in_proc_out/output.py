c29ef6ad1bab2e8b1043109620c0390544d5efbe
Adrianr447 <adrian.rodriguez16@g.austincc.edu>
2025-09-20 22:04:39 +0000

    Add HW02

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,13 @@
-def get_number(num):
-    return num
+TAX_RATE = 0.0675  # 6.75%
+
+def get_sales_tax_amount(meal_amount: float) -> float:
+    """
+    Calculate sales tax for the given meal amount.
+    """
+    return meal_amount * TAX_RATE
+
+def get_tip_amount(meal_amount: float, tip_rate: float) -> float:
+    """
+    Calculate tip for the given meal amount and tip rate.
+    """
+    return meal_amount * tip_rate
